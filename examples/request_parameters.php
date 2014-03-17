e528c24adb1e3bce02e44438651be31c9e336e36
robi50 <fivoneto@outlook.com>
2014-03-17 18:45:30 +0200

    added request parameters example.

---
+++ examples/request_parameters.php
@@ -0,0 +1,18 @@
+<?php
+
+require '../Router.php';
+
+use \Rab\Router;
+
+Router::handle('watch[GET|v=:digit]', function(){
+	return 'video id is: ' . $_GET['v'];
+});
+
+Router::handle('book[POST|key=apple]', function(){
+	return 'book says: hello world!';
+});
+
+Router::handle('book', function(){
+	return 'this is secret book. you must POST the key for read.<br />'.
+	'<form action="" method="POST"><input type="text" name="key" value="apple" /><button>enter!</button></form>';
+});
