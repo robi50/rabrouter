e4596b63040d68d69a25b3cbf09ae92767e3adb9
robi50 <fivoneto@outlook.com>
2014-03-20 19:23:50 +0200

    added basic filter example.

---
+++ examples/basic_filter.php
@@ -0,0 +1,26 @@
+<?php
+
+require '../Router.php';
+
+use \Rab\Router;
+
+// parameter $id type must be number
+Router::filter('NumberTypeFilter', function($id){
+	return preg_match('/^[0-9]+$/', $id);
+}, function($id){
+	return '[NumberTypeFilter]: parameter must be number: ' . $id;
+});
+
+// parameter $id size must be between 5 and 50
+Router::filter('NumberSizeFilter', function($id){
+	$id = intval($id);
+
+	return ($id > 5) && ($id < 50);
+}, function($id){
+	return '[NumberSizeFilter]: number must be between 5 and 50: ' . $id; 
+});
+
+// check NumberTypeFilter and NumberSizeFilter before handle
+Router::handle('number/:any', function($id){
+	return 'number is: ' . $id;
+})->filter('before', 'NumberTypeFilter | NumberSizeFilter');
