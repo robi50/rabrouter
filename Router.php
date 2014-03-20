718ec894c877ad9724baef3cf39dc917453cf454
robi50 <fivoneto@outlook.com>
2014-03-20 20:32:38 +0200

    few comments.

--- Router.php
+++ Router.php
@@ -9,6 +9,14 @@ require 'Handler.php';
 
 class Router{
 
+	/**
+	 * Define router handler.
+	 *
+	 * @param string $pattern // router pattern
+	 * @param mixin $handler // router handler
+	 *
+	 * @return object Handler
+	 */
 	public static function handle($pattern, $handler = null){
 		if(is_array($pattern)){
 			foreach($pattern as $p => $h) new Handler($p, $h);
@@ -17,12 +25,29 @@ class Router{
 		}
 	}
 
+	/**
+	 * Abstract Pattern::define method.
+	 * 
+	 * @return void
+	 */
 	public static function where(){
 		call_user_func_array('\Rab\Pattern::define', func_get_args());
 	}
 
+	/**
+	 * Define router filter or filter group. 	
+	 *
+	 * @param string $name // filter or filter group name
+	 * @param string $controller // filter group pattern
+	 * @param function $controller // filter controller function
+	 * @param function $denied // filter denied function
+	 *
+	 * @return void
+	 */
 	public static function filter($name, $controller, $denied = null){
+		// is filter group name
 		if($name[0] == '*'){
+			// push filter pattern
 			Filter::$filterGroups[substr($name, 1)] = $controller;
 		}else{
 			Filter::$filters[$name] = [$controller, $denied];
